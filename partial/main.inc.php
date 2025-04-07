7525c98710729a27d64b096a3d55e6ee23fa3f2c
giusmili <giusmili67@gmail.com>
2025-04-07 11:25:37 +0200

    php

--- partial/main.inc.php
+++ partial/main.inc.php
@@ -1,10 +1,12 @@
-<figure>
-    <img src="./asset/cover.jpg" alt="cover">
-        <figcaption>
-            <h2>Description de l'article</h2>
-                <p>
-                    No land in sight; we had doubled Cape Clear in the night, and around us was that vast circumference bounded by the line, where water and sky appear to meet. The slate-coloured sea broke in great foamless billows. The 'Great Eastern' struck amidships, and, supported by no sail, rolled frightfully, her bare masts describing immense circles in the air. There was no heaving to speak of, but the rolling was dreadful, it was impossible to stand upright. The officer on watch, clinging to the bridge, 
-                    looked as if he was in a swing.
-                </p>
-        </figcaption>
-</figure>
+
+    <figure>
+        <img src="./asset/cover.jpg" alt="cover">
+            <figcaption>
+                <h2>Description de l'article</h2>
+                    <p>
+                        No land in sight; we had doubled Cape Clear in the night, and around us was that vast circumference bounded by the line, where water and sky appear to meet. The slate-coloured sea broke in great foamless billows. The 'Great Eastern' struck amidships, and, supported by no sail, rolled frightfully, her bare masts describing immense circles in the air. There was no heaving to speak of, but the rolling was dreadful, it was impossible to stand upright. The officer on watch, clinging to the bridge, 
+                        looked as if he was in a swing.
+                    </p>
+    </figcaption>
+    </figure>
+
