f21f13c88c016a07074d684772ded4061eed0147
giusmili <giusmili67@gmail.com>
2025-04-14 11:12:31 +0200

    php controller

---
+++ controller/baseController.php
@@ -0,0 +1,8 @@
+<?php
+    $articles = [
+                "image"=>"./asset/cover.jpg",
+                "title"=>"Description de l'article",
+                "content"=>"No land in sight; we had doubled Cape Clear in the night, and around us was that vast circumference bounded by the line, where water and sky appear to meet. The slate-coloured sea broke in great foamless billows. The 'Great Eastern' struck amidships, and, supported by no sail, rolled frightfully, her bare masts describing immense circles in the air. There was no heaving to speak of, but the rolling was dreadful, it was impossible to stand upright. The officer on watch, clinging to the bridge, 
+                looked as if he was in a swing."
+
+            ];
--- partial/main.inc.php
+++ partial/main.inc.php
@@ -1,12 +1,26 @@
-
+<?php
+    include_once './controller/baseController.php'
+?>
     <figure>
-        <img src="./asset/cover.jpg" alt="cover">
-            <figcaption>
-                <h2>Description de l'article</h2>
-                    <p>
-                        No land in sight; we had doubled Cape Clear in the night, and around us was that vast circumference bounded by the line, where water and sky appear to meet. The slate-coloured sea broke in great foamless billows. The 'Great Eastern' struck amidships, and, supported by no sail, rolled frightfully, her bare masts describing immense circles in the air. There was no heaving to speak of, but the rolling was dreadful, it was impossible to stand upright. The officer on watch, clinging to the bridge, 
-                        looked as if he was in a swing.
-                    </p>
-    </figcaption>
+        <?php
+
+        foreach($articles as $key => $value){
+            if($key == "image"){
+                print "<img src=".$value." alt=\"cover\">";
+
+            }
+            else{
+               return false;
+            }
+            // le texte 
+            echo '<figcaption>
+            <h2>'.$articles['title'].'</h2>
+            <p>'.$articles['content'].'</p>\n
+            </figcaption>';
+         }
+
+
+        ?>
+ 
     </figure>
 
