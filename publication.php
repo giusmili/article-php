2a5b511b9b7617a91cc438255ac88e4c90aee4d9
giusmili <giusmili67@gmail.com>
2025-04-14 17:21:18 +0200

    css table

--- publication.php
+++ publication.php
@@ -6,7 +6,43 @@
     include_once __DIR__ .'/partial/header.inc.php';
     ?>
     <main>
-        <!-- content -->
+        <!-- content table -->
+         <div class="table-primary" role="region" aria-labelledby="table">
+                <table id="table">
+                    <caption>
+                        Classement des articles
+                    </caption>
+                    <thead>
+                        <tr>
+                            <th>Titre</th>
+                            <th>Déscription</th>
+                            <th>Référence</th>
+                            <th>Url</th>
+                        </tr>
+                    </thead>
+                    <tbody> 
+                        <tr>
+                            <td>item</td>
+                            <td>item</td>
+                            <td>item</td>
+                            <td>item</td>
+                        </tr>
+                    </tbody>
+
+                    <tfoot>
+                        <tr>
+                      
+                            <th>Titre</th>
+                            <th>Déscription</th>
+                            <th>Référence</th>
+                            <th>Url</th>
+                    
+                        </tr>
+                    </tfoot>
+
+                </table>
+         </div>
+
     </main>
     <?php
    include_once __DIR__ .'./partial/footer.inc.php';
