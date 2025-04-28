d8762534828cab7904f51ae5a6a0b483fadb349c
giusmili <giusmili67@gmail.com>
2025-04-28 16:53:40 +0200

    model

---
+++ Model/bdd.php
@@ -0,0 +1,15 @@
+<?php
+        # db.php
+$host = 'localhost';
+$dbname = 'post';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+    
+    } catch (PDOException $e) {
+}
